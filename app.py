fc42317872671651e56a64f0b9a226c5100819f2
Ashish Kumar <theashishmavi@outlook.com>
2025-03-13 00:30:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,4 +63,6 @@ def download_file():
     return "File not found."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))  # Render assigns the PORT dynamically
+    app.run(host='0.0.0.0', port=port)
